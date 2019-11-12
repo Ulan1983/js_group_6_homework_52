30f8537c852157e72b6f53ffea3878d2262a77f8
Ulan Beltaev <ulanbeltaev@gmail.com>
2019-11-12 17:42:03 +0600

    Added Circle.js file

--- task-1/src/App.js
+++ task-1/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Circle from './components/Circle/Circle'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numbers: []
+  };
+
+  generateNumbers = () => {
+    let numbersArray = [];
+    const min = 5;
+    const max = 36;
+    for (let i = 0; i < 5;) {
+      const number = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (!numbersArray.includes(number)) {
+        numbersArray.push(number);
+      };
+    }
+    numbersArray.sort((a, b) => a - b);
+    this.setState({ numbersArray });
+  };
+
+  getTotal() {
+    this.state.numbers.map(this.generateNumbers())
+  };
+
+
+
+  render() {
+    return (
+      <div className="App" >
+        <button onClick={this.generateNumbers}>Show numbers</button>
+        <div>{this.getTotal()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
